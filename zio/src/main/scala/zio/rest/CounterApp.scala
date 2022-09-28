f21b46503709aca4b3db0bf94cb55bb60acfb4da
Jaroslaw Zawila <jaroslaw.zawila@copper.co>
2022-09-28 01:17:58 +0100

    Add counter app

--- zio/src/main/scala/zio/rest/CounterApp.scala
+++ zio/src/main/scala/zio/rest/CounterApp.scala
@@ -1,9 +1,25 @@
 package zio.rest
 
-import zhttp.http.{Http, Request, Response}
-import zio.Ref
+import zhttp.http._
+import zio._
 
-class Counter {
-  def apply: Http[Ref[Int], Nothing, Request, Response] = ???
+object CounterApp {
+  def apply(): Http[Ref[Int], Nothing, Request, Response] =
+    Http.fromZIO(ZIO.service[Ref[Int]]).flatMap { ref =>
+      Http.collectZIO[Request] {
+        case Method.GET -> !! / "up" =>
+          ref
+            .updateAndGet(_ + 1)
+            .map(_.toString)
+            .map(Response.text)
+        case Method.GET -> !! / "down" =>
+          ref
+            .updateAndGet(_ - 1)
+            .map(_.toString)
+            .map(Response.text)
+        case Method.GET -> !! / "get" =>
+          ref.get.map(_.toString).map(Response.text)
+      }
+    }
 
 }
