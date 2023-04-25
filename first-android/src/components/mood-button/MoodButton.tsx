7d4eab596ae9d582d7894553ff6846ecd2d0b031
Jarek Zawila <jaroslaw.zawila@gmail.com>
2023-04-25 07:52:21 +0100

    Remove first mobile app

--- first-android/src/components/mood-button/MoodButton.tsx
+++
@@ -1,9 +0,0 @@
-import {Component} from "react";
-
-export class MoodButton extends Component<any, any> {
-    render() {
-        return (
-            <div>Good morning</div>
-        );
-    }
-}
